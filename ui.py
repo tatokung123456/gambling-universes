3cbd361bbcc5fd7edbf001455fbcd5734db323dc
tatokung123456 <33756704+tatokung123456@users.noreply.github.com>
2025-07-13 00:02:14 +0700

    Update ui.py

--- ui.py
+++ ui.py
@@ -1,1 +1,20 @@
+import tkinter as tk
+from logic import load_choices, random_choice
 
+def choose():
+    result.set(random_choice(choices))
+
+choices = load_choices("data/choices.txt")
+
+root = tk.Tk()
+root.title("ตัวสุ่มของคุณ")
+root.geometry("300x200")
+
+result = tk.StringVar()
+label = tk.Label(root, textvariable=result, font=("Arial", 20))
+label.pack(pady=20)
+
+btn = tk.Button(root, text="สุ่ม!", command=choose)
+btn.pack()
+
+root.mainloop()
